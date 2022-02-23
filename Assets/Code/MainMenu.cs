3f1b5b0909f05d7ed05b7bd1e51a4f25aac6b107
gpark31 <55361125+gpark31@users.noreply.github.com>
2022-02-22 20:18:53 -0500

    levels

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(int l)
+    {
+        SceneManager.LoadScene(l);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
