3ac7815b4c01790d811be230fde1904cc6bc23d6
gpark31 <55361125+gpark31@users.noreply.github.com>
2022-02-27 01:15:59 -0500

    testing improved

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(int l)
     {
         SceneManager.LoadScene(l);
+        if(SceneManager.GetActiveScene() != SceneManager.GetSceneAt(l))
+        {
+            Debug.Log("Error");
+        }
     }
 
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneAt(0))
+        {
+            Debug.Log("Error");
+        }
     }
 
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneAt(SceneManager.GetActiveScene().buildIndex + 1))
+        {
+            Debug.Log("Error");
+        }
     }
 
     public void QuitGame()
