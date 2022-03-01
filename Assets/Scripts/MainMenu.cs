6b971068b1634a91c47154b07704c90efe369756
gpark31 <55361125+gpark31@users.noreply.github.com>
2022-03-01 15:11:41 -0500

    no bugs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,28 +9,17 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(int l)
     {
         SceneManager.LoadScene(l);
-        //if (SceneManager.GetActiveScene() != SceneManager.GetSceneAt(l))
-        //{
-       //     Debug.Log("Error");
-       // }
+ 
     }
 
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(0);
-        if (SceneManager.GetActiveScene() != SceneManager.GetSceneAt(0))
-        {
-            Debug.Log("Error");
-        }
     }
 
     public void NextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        if (SceneManager.GetActiveScene() != SceneManager.GetSceneAt(SceneManager.GetActiveScene().buildIndex + 1))
-        {
-            Debug.Log("Error");
-        }
     }
 
     public void QuitGame()
