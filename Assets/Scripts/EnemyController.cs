92f6c62c2d6c29ceba7b4488b1d4e3c5437ef99b
gpark31 <55361125+gpark31@users.noreply.github.com>
2022-03-20 22:31:08 -0400

    Enemy Patrol

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 1f;
+    Rigidbody2D rb;
+    bool right = true;
+
+    // Start is cddalled before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= 100f && transform.position.x < 1420f &&  right == true)
+        {
+            rb.velocity = new Vector2(speed, 0f);
+        }
+        if(transform.position.x >= 1420f && right == true)
+        {
+            right = false;
+            transform.Rotate(0, 180, 0);
+            rb.velocity = new Vector2(-speed, 0f);
+        }
+        if (transform.position.x <= 1420f && transform.position.x > 100f && right == false)
+        {
+            rb.velocity = new Vector2(-speed, 0f);
+        }
+        if(transform.position.x <= 100f && right == false)
+        {
+            right = true;
+            transform.Rotate(0, 180, 0);
+            rb.velocity = new Vector2(speed, 0f);
+
+        }
+    }
+  
+}
