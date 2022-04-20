e7ac9ecc921be8aac9cbf649790605285f087ad5
gpark31 <55361125+gpark31@users.noreply.github.com>
2022-04-19 21:16:27 -0400

    fixed error

--- Assets/Scripts/audio.cs
+++ Assets/Scripts/audio.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class audio : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
-
+    
     // Start is called before the first frame update
     void Start()
     {
